d699001a871a36ecff809259f9fc454e2d83cc1b
Sunson <91197035+huuson889@users.noreply.github.com>
2024-03-20 22:04:50 +0700

    Update rc1blogger.js

--- rc1blogger.js
+++ rc1blogger.js
@@ -1,27 +1,70 @@
+//Tomato Project
 function showrecentcomments(json) {
   for (var i = 0; i < a_rc; i++) {
-    var entry = json.feed.entry[i];
-    var commentLink = entry.link.find(link => link.rel == 'alternate').href;
-    commentLink = commentLink.replace("#", "#comment-");
-    var postLink = commentLink.split("#")[0];
-    var postTitleElement = entry.content.$t.match(/<span\s+class="post-title">(.*?)<\/span>/);
-    var postTitle = postTitleElement ? postTitleElement[1] : '';
-    var commentContent = entry.content ? entry.content.$t : entry.summary ? entry.summary.$t : "";
-    commentContent = commentContent.replace(/<\S[^>]*>/g, "");
-    document.write('<div class="rcw-comments">');
-    document.write(`<a href="${commentLink}">${entry.author[0].name.$t}</a> vừa bình luận`);
-    if (n_rc) document.write(` tại <a href="${postLink}">${postTitle}</a>`);
-    if (o_rc > 0) {
-      document.write(": ");
-      if (commentContent.length < o_rc) {
-        document.write(`<i>${commentContent}</i>`);
-      } else {
-        var trimmedContent = commentContent.substring(0, o_rc);
-        var lastSpace = trimmedContent.lastIndexOf(" ");
-        trimmedContent = trimmedContent.substring(0, lastSpace);
-        document.write(`<i>${trimmedContent}&hellip;</i>`);
+    var b_rc = json.feed.entry[i];
+    var c_rc;
+    if (i == json.feed.entry.length) break;
+    for (var k = 0; k < b_rc.link.length; k++) {
+      if (b_rc.link[k].rel == 'alternate') {
+        c_rc = b_rc.link[k].href;
+        break;
       }
     }
-    document.write("</div>");
+    c_rc = c_rc.replace("#", "#comment-");
+    var d_rc = c_rc.split("#");
+    d_rc = d_rc[0];
+    var e_rc = d_rc.split("/");
+    e_rc = e_rc[5];
+    e_rc = e_rc.split(".html");
+    e_rc = e_rc[0];
+    
+    // Get the real post title
+    var postTitle = b_rc.title.$t;
+
+    var g_rc = b_rc.published.$t;
+    var h_rc = g_rc.substring(0, 4);
+    var i_rc = g_rc.substring(5, 7);
+    var j_rc = g_rc.substring(8, 10);
+    var k_rc = new Array();
+    k_rc[1] = "Jan";
+    k_rc[2] = "Feb";
+    k_rc[3] = "Mar";
+    k_rc[4] = "Apr";
+    k_rc[5] = "May";
+    k_rc[6] = "Jun";
+    k_rc[7] = "Jul";
+    k_rc[8] = "Aug";
+    k_rc[9] = "Sep";
+    k_rc[10] = "Oct";
+    k_rc[11] = "Nov";
+    k_rc[12] = "Dec";
+    if ("content" in b_rc) {
+      var l_rc = b_rc.content.$t;
+    } else if ("summary" in b_rc) {
+      var l_rc = b_rc.summary.$t;
+    } else var l_rc = "";
+    var re = /<\S[^>]*>/g;
+    l_rc = l_rc.replace(re, "");
+    document.write('<div class="rcw-comments">');
+    if (m_rc == true) document.write('tại ' + k_rc[parseInt(i_rc, 10)] + ' ' + j_rc + ' ');
+    document.write('<a href="' + c_rc + '">' + b_rc.author[0].name.$t + '</a> đã bình luận');
+    if (n_rc == true) document.write(' tại ' + postTitle); // Use the real post title here
+    if (o_rc == 0) {
+      document.write('</div>');
+    } else {
+      document.write(': ');
+      if (l_rc.length < o_rc) {
+        document.write('<i>');
+        document.write(l_rc);
+        document.write('</i></div>');
+      } else {
+        document.write('<i>');
+        l_rc = l_rc.substring(0, o_rc);
+        var p_rc = l_rc.lastIndexOf(" ");
+        l_rc = l_rc.substring(0, p_rc);
+        document.write(l_rc + '&hellip;</i></div>');
+        document.write('');
+      }
+    }
   }
 }
