d4d6175aac2a628b20a85f0043075e4a3ad8d4cd
Sunson <91197035+huuson889@users.noreply.github.com>
2024-03-20 21:37:35 +0700

    Update rc1blogger.js

--- rc1blogger.js
+++ rc1blogger.js
@@ -3,29 +3,27 @@
 function showrecentcomments(json) {
   for (var i = 0; i < a_rc; i++) {
     var entry = json.feed.entry[i];
-    var commentLink = entry.link.find(link => link.rel == 'alternate').href;
-    commentLink = commentLink.replace("#", "#comment-");
-    var postLink = commentLink.split("#")[0];
-    var postTitle = entry.title.$t.split(" on ")[1];
-    var publishedDate = new Date(entry.published.$t);
-    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    var commentContent = entry.content ? entry.content.$t : entry.summary ? entry.summary.$t : "";
-    commentContent = commentContent.replace(/<\S[^>]*>/g, "");
-    document.write('<div class="rcw-comments">');
-    if (m_rc) document.write(`tại ${months[publishedDate.getMonth()]} ${publishedDate.getDate()} `);
-    document.write(`<a href="${commentLink}">${entry.author[0].name.$t}</a> vừa bình luận`);
-    if (n_rc) document.write(` tại <a href="${postLink}">${postTitle}</a>`);
-    if (o_rc > 0) {
-      document.write(": ");
-      if (commentContent.length < o_rc) {
-        document.write(`<i>${commentContent}</i>`);
-      } else {
-        var trimmedContent = commentContent.substring(0, o_rc);
-        var lastSpace = trimmedContent.lastIndexOf(" ");
-        trimmedContent = trimmedContent.substring(0, lastSpace);
-        document.write(`<i>${trimmedContent}&hellip;</i>`);
+    var commentLink;
+    for (var k = 0; k < entry.link.length; k++) {
+      if (entry.link[k].rel == 'alternate') {
+        commentLink = entry.link[k].href;
+        break;
       }
     }
-    document.write("</div>");
+    commentLink = commentLink.replace("#", "#comment-");
+    var postLink = commentLink.split("#")[0];
+    //var postTitle = entry.title.$t;
+    var publishedDate = entry.published.$t;
+    var year = publishedDate.substring(0, 4);
+    var month = publishedDate.substring(5, 7);
+    var day = publishedDate.substring(8, 10);
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var commentContent = ("content" in entry) ? entry.content.$t : ("summary" in entry) ? entry.summary.$t : "";
+    commentContent = commentContent.replace(/<\S[^>]*>/g, "");
+    document.write('<div class="rcw-comments">');
+    if (m_rc == true) document.write('ở ' + months[parseInt(month, 10) - 1] + ' ' + day + ' ');
+    document.write('<a style="color: #212121;" href="' + commentLink + '">' + entry.author[0].name.$t + '</a> vừa bình luận rằng ');
+    if (n_rc == true) document.write('<a href="' + postLink + '">' + <data:post.title/> + '</a>');
+    document.write('</div>');
   }
 }
