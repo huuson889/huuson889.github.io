8ddbee3264cb8c194a92cb6ea33b26959abe91d7
Sunson <91197035+huuson889@users.noreply.github.com>
2024-03-20 22:34:52 +0700

    Update rc1blogger.js

--- rc1blogger.js
+++ rc1blogger.js
@@ -19,6 +19,23 @@ function showrecentcomments(json) {
         e_rc = e_rc[0];
         var f_rc = e_rc.replace(/-/g, " ");
         f_rc = f_rc.link(d_rc);
+        var g_rc = b_rc.published.$t;
+        var h_rc = g_rc.substring(0, 4);
+        var i_rc = g_rc.substring(5, 7);
+        var j_rc = g_rc.substring(8, 10);
+        var k_rc = new Array();
+        k_rc[1] = "Jan";
+        k_rc[2] = "Feb";
+        k_rc[3] = "Mar";
+        k_rc[4] = "Apr";
+        k_rc[5] = "May";
+        k_rc[6] = "Jun";
+        k_rc[7] = "Jul";
+        k_rc[8] = "Aug";
+        k_rc[9] = "Sep";
+        k_rc[10] = "Oct";
+        k_rc[11] = "Nov";
+        k_rc[12] = "Dec";
         if ("content" in b_rc) {
             var l_rc = b_rc.content.$t;
         } else if ("summary" in b_rc) {
@@ -27,19 +44,29 @@ function showrecentcomments(json) {
         var re = /<\S[^>]*>/g;
         l_rc = l_rc.replace(re, "");
         document.write('<div class="rcw-comments">');
+        if (m_rc == true) document.write('tại ' + k_rc[parseInt(i_rc, 10)] + ' ' + j_rc + ' ');
         document.write('<a href="' + c_rc + '">' + b_rc.author[0].name.$t + '</a> đã bình luận');
-        document.write(': ');
-        if (l_rc.length < o_rc) {
-            document.write('<i>');
-            document.write(l_rc);
-            document.write('</i></div>');
+        
+        // Fetching Post Title
+        var postTitle = b_rc.title.$t;
+        document.write(' trên bài viết: ' + postTitle); // Modified line
+        
+        if (o_rc == 0) {
+            document.write('</div>');
         } else {
-            document.write('<i>');
-            l_rc = l_rc.substring(0, o_rc);
-            var p_rc = l_rc.lastIndexOf(" ");
-            l_rc = l_rc.substring(0, p_rc);
-            document.write(l_rc + '&hellip;</i></div>');
-            document.write('');
+            document.write(': ');
+            if (l_rc.length < o_rc) {
+                document.write('<i>');
+                document.write(l_rc);
+                document.write('</i></div>');
+            } else {
+                document.write('<i>');
+                l_rc = l_rc.substring(0, o_rc);
+                var p_rc = l_rc.lastIndexOf(" ");
+                l_rc = l_rc.substring(0, p_rc);
+                document.write(l_rc + '&hellip;</i></div>');
+                document.write('');
+            }
         }
     }
 }
