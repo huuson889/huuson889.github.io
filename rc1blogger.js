ea43ca3dc0fe53340c01fabaaa5b4a44bf1981c2
Sunson <91197035+huuson889@users.noreply.github.com>
2024-03-20 22:13:54 +0700

    Update rc1blogger.js

--- rc1blogger.js
+++ rc1blogger.js
@@ -17,27 +17,10 @@ function showrecentcomments(json) {
     e_rc = e_rc[5];
     e_rc = e_rc.split(".html");
     e_rc = e_rc[0];
-    
-    // Get the real post title
+
+    // Get the real post title from b_rc.title.$t
     var postTitle = b_rc.title.$t;
 
-    var g_rc = b_rc.published.$t;
-    var h_rc = g_rc.substring(0, 4);
-    var i_rc = g_rc.substring(5, 7);
-    var j_rc = g_rc.substring(8, 10);
-    var k_rc = new Array();
-    k_rc[1] = "Jan";
-    k_rc[2] = "Feb";
-    k_rc[3] = "Mar";
-    k_rc[4] = "Apr";
-    k_rc[5] = "May";
-    k_rc[6] = "Jun";
-    k_rc[7] = "Jul";
-    k_rc[8] = "Aug";
-    k_rc[9] = "Sep";
-    k_rc[10] = "Oct";
-    k_rc[11] = "Nov";
-    k_rc[12] = "Dec";
     if ("content" in b_rc) {
       var l_rc = b_rc.content.$t;
     } else if ("summary" in b_rc) {
@@ -46,9 +29,8 @@ function showrecentcomments(json) {
     var re = /<\S[^>]*>/g;
     l_rc = l_rc.replace(re, "");
     document.write('<div class="rcw-comments">');
-    if (m_rc == true) document.write('tại ' + k_rc[parseInt(i_rc, 10)] + ' ' + j_rc + ' ');
     document.write('<a href="' + c_rc + '">' + b_rc.author[0].name.$t + '</a> đã bình luận');
-    if (n_rc == true) document.write(' tại ' + postTitle); // Use the real post title here
+    if (n_rc == true) document.write(' tại ' + postTitle); // Use the real post title
     if (o_rc == 0) {
       document.write('</div>');
     } else {
