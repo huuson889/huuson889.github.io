bc8eacbd794bdbecda4a95d070471601e159a4f0
Sunson <91197035+huuson889@users.noreply.github.com>
2025-11-18 08:34:35 +0700

    Update Noel.js

--- Noel-decor/Noel.js
+++ Noel-decor/Noel.js
@@ -6,7 +6,7 @@ var no=20;var hidesnowtime=0;var snowdistance='pageheight';var ie4up=(document.a
 /*Code gốc trang trí ông già Noel tại (có dùng claude.ai chuyển đổi): https://www.tranbadat.com/2015/12/trang-tri-giang-sinh-voi-ong-gia-noel-cho-blogspot.html*/
 // santa-animation.js
 document.addEventListener('DOMContentLoaded', function() {
-  document.body.innerHTML += '<img id="halo" title="Happy Noel" style="cursor:pointer;position:fixed;z-index:99999" height="120" src="https://lh3.ggpht.com/-LSDhJFNSG-E/VnQh0rSGAHI/AAAAAAAADoo/3FdK8o-hZ6A/s1600/tuan_loc_cho_qua_cua_ong_gia_noel_anh.gif"/>';
+  document.body.innerHTML += '<img id="halo" title="Happy Noel" style="cursor:pointer;position:fixed;z-index:99999" height="80" src="https:huuson889.github.io/Noel-decor/ong-gia.gif"/>';
   
   var halo = document.getElementById('halo');
   
@@ -19,8 +19,9 @@ document.addEventListener('DOMContentLoaded', function() {
   }, 5000);
   
   halo.onclick = function() {
-    window.open('https://lh3.ggpht.com/-LSDhJFNSG-E/VnQh0rSGAHI/AAAAAAAADoo/3FdK8o-hZ6A/s1600/tuan_loc_cho_qua_cua_ong_gia_noel_anh.gif', '');
+    window.open('https://huuson889.github.io/Noel-decor/ong-gia.gif', '');
   };
 });
+
 
 
