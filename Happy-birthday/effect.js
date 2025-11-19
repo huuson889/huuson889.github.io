d87cfe95c3dcc4101cb3ac54a0196ebc4333b6dc
Sunson <91197035+huuson889@users.noreply.github.com>
2025-11-19 19:04:59 +0700

    Update effect.js

--- Happy-birthday/effect.js
+++ Happy-birthday/effect.js
@@ -52,7 +52,7 @@ $('document').ready(function(){
 	});
 
 	function loopOne() {
-		var randleft = 1000*Math.random();
+		var randleft = window.innerWidth * Math.random();
 		var randtop = 500*Math.random();
 		$('#b1').animate({left:randleft,bottom:randtop},10000,function(){
 			loopOne();
